e42e23dfc542ef3342d7fd25102f5a8093d620ba
IAN ROOT <ianaroot@gmail.com>
2023-11-20 14:45:48 -0800

    first commit!

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+
+    def index 
+        logger.debug "YES! GOD DAMNIT. THAT IS RIGHT. I DID IT. ARE YOU KIDDING ME? THAT'S RIGHT. WHO DO YOU THINK YOU ARE? I AM!"
+    end
+
+    def default_url_options
+    end
+end
