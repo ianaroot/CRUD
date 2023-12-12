fa13cdd40983cc24a4e3d7933fff1ffc69371143
IAN ROOT <ianaroot@gmail.com>
2023-12-11 16:34:45 -0800

    wip: devise issues

---
+++ db/migrate/20231118035555_devise_create_users.rb
@@ -0,0 +1,36 @@
+class DeviseCreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+
+      t.string :name
+      t.string :last_login
+
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      #devise stuff from digital ocean tutorial, leaving commented out because i probably won't use trackable or confirmable
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      t.timestamps
+    
+    end
+  end
+end
